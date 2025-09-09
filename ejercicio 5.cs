b9ab45da42c6ff5060adb78e53e5abd919daec5b
emilyvillanueva226-cell <emilyvillanueva226@gmail.com>
2025-09-09 08:56:49 -0500

    Add files via upload

---
+++ ejercicio 5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incremento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.Write("Ingrese su edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad <= 17)
+            {
+                Console.WriteLine("Usted puede acceder al area de pediatria");
+            }
+            else
+            {
+                Console.WriteLine("Usted ya no puede acceder al area de pediatria");
+            }
+        }
+    }
+}
