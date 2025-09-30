92a49080ca0fc30a108081538db53260f13701aa
R3lAYZ <bryanmat9@gmail.com>
2025-09-30 10:36:52 -0500

    Add files via upload
    AGREGO EL EJERCICIO 2

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            {
+
+                int opcion;
+
+
+
+                do
+
+                {
+
+                    Console.WriteLine("MENÚ DE INICIO");
+                    Console.WriteLine("1. Calcular Suma");
+                    Console.WriteLine("2. Calcular Resta");
+                    Console.WriteLine("3. Salir");
+                    Console.Write("Ingrese una opción (1-3): ");
+
+                    opcion = Convert.ToInt32(Console.ReadLine());
+
+                    if (opcion < 1 || opcion > 3)
+
+                    {
+
+                        Console.WriteLine(" Opción inválida. Intente nuevamente.\n");
+
+                    }
+
+
+                } while (opcion < 1 || opcion > 3);
+               
+
+                if (opcion == 1)
+                    Console.WriteLine("Has elegido: Calcular Suma");
+
+                else if (opcion == 2)
+                    Console.WriteLine("Has elegido: Calcular Resta");
+
+                else
+
+                    Console.WriteLine("Has elegido: Salir");
+
+
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+
+
+    }
+
+}
+    
+
