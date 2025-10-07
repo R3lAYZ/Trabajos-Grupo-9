afefe55175cbe72c04249438a716fc99efca7205
R3lAYZ <bryanmat9@gmail.com>
2025-10-07 09:45:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_2
+{
+    internal class Program
+    {
+        static int exponente(int n, int expo)
+        {
+            int f = 1;
+            if (expo == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= expo; i++)
+                {
+                    f = f * n;
+                }
+                return f;
+
+
+            }
+        }
+    }
+}
