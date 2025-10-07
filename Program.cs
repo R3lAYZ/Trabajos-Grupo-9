6e544e349a1022779c4c4578bc0b7259885a0487
emilyvillanueva226-cell <emilyvillanueva226@gmail.com>
2025-10-07 10:55:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               ejercicio6.ingreso();
+              ejercicio6.sumar();
+            ejercicio6.imprimir();
+                Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ ejercicio6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio6
+{
+    internal class ejercicio6
+    {
+        public static int n1;
+        public static int n2;
+        public static int s;
+        public static void ingreso()
+        {
+            Console.Write("Ingresar N1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar N2: ");
+            n2 = int.Parse(Console.ReadLine());
+        }
+        public static void sumar()
+        {
+            s = n1 + n2;
+        }
+        public static void imprimir()
+        {
+            Console.WriteLine("La suma es: {0}", s);
+        }
+
+        
+    }
+}
+        
+    
+
+
+
