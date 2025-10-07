1cb4937539f5b5883d51ffde97cfcd569244fbeb
emilyvillanueva226-cell <emilyvillanueva226@gmail.com>
2025-10-07 11:53:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerrcicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+                int a = 10, b = 10;
+                ejercicio7.incremento1(a);
+                Console.WriteLine("a = {0}", a);
+                ejercicio7.incremento2(ref b);
+                Console.WriteLine("b = {0}", b);
+                Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ ejercicio7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerrcicio7
+{
+    internal class ejercicio7
+    {
+
+        public static void incremento1(int n1)
+        {
+            n1 = n1 + 10;
+        }
+        public static void incremento2(ref int n1)
+        {
+            n1 = n1 + 10;
+        }
+        
+    }
+}
+
+    
+
