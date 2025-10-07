873f198e7d4892986a0d3a72f5705a8c7e6af578
R3lAYZ <bryanmat9@gmail.com>
2025-10-07 09:51:35 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_2
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int n, e, r;
+
+            Console.Write("Ingresar numero: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingresar exponente: ");
+            e = int.Parse(Console.ReadLine());
+
+            r = Exponente(n, e); 
+
+            Console.Write("Resultado = {0}", r);
+            Console.ReadKey();
+        }
+
+        static int Exponente(int numero, int exponente)
+        {
+            int resultado = 1;
+            for (int i = 0; i < exponente; i++)
+            {
+                resultado *= numero; 
+            }
+            return resultado;
+        }
+    }
+}
